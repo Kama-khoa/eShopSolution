7ef26ce11242be278953ff4577515cb3929b9d51
Trần Khoa <khoa68471@gmail.com>
2024-07-31 19:11:54 +0700

    Add Entity

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## Technologies
 - ASP.Net Core 8.0
 - Entity Framework Core 8.0
+## Install Packages
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
 ## Youtube turtorial
 ## How to configure and run
 ## How to contribute
---
+++ eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public int DateCreated { get; set; }
+        public int SeoAlias { get; set; }
+    }
+}
